5bc0b829002438dece2869a891daa6a03d978cce
RatlyREM <39582908+RatlyREM@users.noreply.github.com>
2024-04-24 16:55:27 +0900

    [Silver IV] Title: 수 이어 쓰기 1, Time: 124 ms, Memory: 14152 KB -BaekjoonHub

---
+++ 백준/Silver/1748. 수 이어 쓰기 1/수 이어 쓰기 1.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(br.readLine());
+		
+		int check = 9;
+		int total = 0;
+		int count = 1;
+		
+		while (true) {
+			if(check > N) {
+				//마지막 처리 필요
+				total += N * count;
+				break;
+			}
+			
+			total += count*check;
+			N -= check;
+
+			check *=10;
+			count++;	
+			
+		}
+		
+		System.out.println(total);
+		
+	}
+}
