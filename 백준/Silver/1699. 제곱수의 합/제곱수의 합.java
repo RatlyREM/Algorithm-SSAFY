a355f9a0d91432455385c410c9665526c7c464f6
RatlyREM <39582908+RatlyREM@users.noreply.github.com>
2024-04-25 10:30:13 +0900

    [Silver II] Title: 제곱수의 합, Time: 168 ms, Memory: 14812 KB -BaekjoonHub

--- 백준/Silver/1699. 제곱수의 합/제곱수의 합.java
+++ 백준/Silver/1699. 제곱수의 합/제곱수의 합.java
@@ -1,8 +1,4 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.Iterator;
+import java.io.*;
 
 public class Main {
 	public static void main(String[] args) throws NumberFormatException, IOException {
@@ -11,22 +7,14 @@ public class Main {
 		int N = Integer.parseInt(br.readLine());
 		
 		int[] DP = new int[N+1];
-		
-		DP[1] = 1;
-		//DP[2] = 2;
-		//DP[3] = 3;
-		
 		int count = 1;
+		
 		//전처리
 		while(count*count <= N) {
 			DP[count*count] =1;
-			
 			count++;
 		}
-		
-		//System.out.println(Arrays.toString(DP));
-		
-		//int zegob = 2;
+
 		for (int i = 1; i <= N; i++) {
 			int zegob = 1;
 			int min = Integer.MAX_VALUE;
@@ -45,10 +33,7 @@ public class Main {
 			DP[i] = min;
 			
 		}
-		
-		
+
 		System.out.println(DP[N]);
-		
-		
 	}
 }
