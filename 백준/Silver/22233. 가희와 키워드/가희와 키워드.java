5770a46af3c420b531ef81a09431105465042361
RatlyREM <39582908+RatlyREM@users.noreply.github.com>
2024-05-09 09:43:26 +0900

    [Silver II] Title: 가희와 키워드, Time: 1272 ms, Memory: 229432 KB -BaekjoonHub

--- 백준/Silver/22233. 가희와 키워드/가희와 키워드.java
+++ 백준/Silver/22233. 가희와 키워드/가희와 키워드.java
@@ -3,28 +3,20 @@ import java.util.*;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
-		/*
-		 * 1. 쉼표 기준으로 쓴 글 나누기
-		 * 		키워드들 map에 삽입!!
-		 * 2. 키워드들 돌면서, map에 존재하는 키워드라면? -> map에서 빼기
-		 * 		map에 존재하지 않는 키워드라면? -> 그냥 넘기기
-		 * 
-		 * 현재 map의 크기 출력하면 됨.
-		 */
-		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		
 		
 		int N = Integer.parseInt(st.nextToken());
 		int M = Integer.parseInt(st.nextToken());
 		
-		HashMap<String, Boolean> hm = new HashMap<String, Boolean>();
-		
+		HashSet<String> hs = new HashSet<String>();
+
 		for (int i = 0; i < N; i++) {
 			String s = br.readLine();
 			
-			hm.put(s, true);
+			hs.add(s);
 		}
 		
 		for (int i = 0; i < M; i++) {
@@ -32,12 +24,15 @@ public class Main {
 			String[] tempS = s.split(",");
 			
 			for(String t : tempS) {
-				if(hm.containsKey(t)) {
-					hm.remove(t);
+				if(hs.contains(t)) {
+					hs.remove(t);
 				}
 			}
-			
-			System.out.println(hm.size());
+
+			bw.write(Integer.toString(hs.size()) + "\n");
 		}
+		
+		bw.flush();
+		bw.close();
 	}
 }
