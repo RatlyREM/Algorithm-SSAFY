a646a610be2feb191a69ddcf0af7459d52d15e3e
RatlyREM <39582908+RatlyREM@users.noreply.github.com>
2024-06-29 05:10:55 +0900

    [Gold V] Title: 가장 긴 짝수 연속한 부분 수열 (large), Time: 432 ms, Memory: 91288 KB -BaekjoonHub

--- 백준/Gold/22862. 가장 긴 짝수 연속한 부분 수열 （large）/가장 긴 짝수 연속한 부분 수열 （large）.java
+++ 백준/Gold/22862. 가장 긴 짝수 연속한 부분 수열 （large）/가장 긴 짝수 연속한 부분 수열 （large）.java
@@ -1,55 +1,67 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
- 
- 
- 
+import java.util.StringTokenizer;
+
 public class Main {
-    static int n, k;
-    static int [] arr;
-    static int max;
-    static int count;
-    static boolean [] check;
-    static int first=1, second=1;
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String[] s = br.readLine().split(" ");
-        n = Integer.parseInt(s[0]);
-        k = Integer.parseInt(s[1]);
-        check = new boolean[n+1];
-        arr= new int[n+1];
-        String[] s1 = br.readLine().split(" ");
-        for(int i=1; i<=n; i++){
-            int num = Integer.parseInt(s1[i-1]);
-            arr[i]=num;
-            if(num%2!=1){
-                check[i]=true;
-            }
-        }
-        solve();
-        System.out.println(max);
-    }
- 
-    public static void solve(){
-        while (second<=n){
-            if(count<k){ // count<k 를 만족할 때까지 계속해서 count를 증가하며 수열의 길이를 증가시킬 수 있다. 
-                if(!check[second]){
-                    count++;
-                }
-                second++;
-                max = Math.max(max,second-first-count);
-            }
-            else if(check[second]){ // count==K를 만족하지만 뒤 숫자가 짝수여서 더 길게 가능하다.
-                second++;
-                max = Math.max(max,second-first-count);
-            }
-            else if(count==k && !check[second]){ // count==K가 됐으며 뒤의 수도 홀수일 때 first를 증가시킵니다. 
-                if(!check[first]){ // first가 있던 위치의 수가 홀수일 때 count를 낮추며 아닌 경우에는 count는 냅둡니다.
-                    count--;
-                }
-                first++;
-            }
-        }
-    }
+	public static void main(String[] args) throws IOException {
+		
+		/*
+		 * 홀수다? -> 없앨 기회가 있다? -> 없애기
+			짝수다? -> 그냥 나아간다(짝수 수열길이 플러스)
+			홀수다? -> 없앨 기회가 없다? -> 이전까지의 길이 최댓값 검사, s부터 홀수 나올때까지 더하기  
+		 */
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		StringTokenizer st = new  StringTokenizer(br.readLine());
+		
+		
+		int N = Integer.parseInt(st.nextToken());
+		int K = Integer.parseInt(st.nextToken());
+		int[] arr = new int[N];
+		
+		st = new StringTokenizer(br.readLine());
+		
+		for (int i = 0; i < N; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		int start = 0, end=0;
+		int chance = 0;
+		int oddLength = 0;
+		int maxLength = -1;
+		
+		
+		while(true) {
+			//System.out.println(start + " " + end);
+			
+			if(chance > K) {
+				if(arr[start] % 2 == 1) {
+					chance--;
+				}
+				start++;
+			}
+			else if(end == N) {
+				break;
+			}
+			else {
+				if(arr[end] %2 == 1) {
+					chance++;
+				}
+				
+				
+				end++;
+			}
+			
+			if(chance <= K) {
+				maxLength = Integer.max(maxLength, end-start-chance);
+			}
+			//System.out.println("chance: " + chance);
+			//System.out.println("쵀대: " + (end-start-chance) + " " + maxLength);
+		}
+		
+		System.out.println(maxLength);
+		
+	}
 }
