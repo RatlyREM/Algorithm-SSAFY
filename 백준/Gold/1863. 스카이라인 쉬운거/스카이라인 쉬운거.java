e4dc8e21e9bf7f34b0aeafed8d1942705187bcf1
RatlyREM <39582908+RatlyREM@users.noreply.github.com>
2024-07-07 17:36:07 +0900

    [Gold IV] Title: 스카이라인 쉬운거, Time: 176 ms, Memory: 19552 KB -BaekjoonHub

--- 백준/Gold/1863. 스카이라인 쉬운거/스카이라인 쉬운거.java
+++ 백준/Gold/1863. 스카이라인 쉬운거/스카이라인 쉬운거.java
@@ -3,70 +3,50 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+	static int count = 0;
+	static Stack<Point> stack;
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
 		int n = Integer.parseInt(br.readLine());
 		StringTokenizer st;
-		
-		int height = 0;
-		Stack<Point> stack = new Stack<Point>();
-		int count = 0;
-		
+		stack = new Stack<Point>();
+
 		for (int i = 0; i < n; i++) {
 			st = new StringTokenizer(br.readLine());
 			
 			int x  =Integer.parseInt(st.nextToken());
 			int y  =Integer.parseInt(st.nextToken());
 			
-			while(true) {
-				if(stack.isEmpty()) {
-					stack.add(new Point(x,y));
-					height = y;
-					break;
-				} else {
-					if(stack.peek().y < y) {
-						stack.add(new Point(x,y));
-						height = y;
-						break;
-					}
-					else if(stack.peek().y == y) {
-						break;
-					}
-					else {
-						height = x;
-						stack.pop();
-						count++;
-					}
-				}
-			}
+			check(x,y);
 		}
 		
 		int x  = n;
 		int y = 0;
 		
+		check(x,y);
+		
+		System.out.println(count);
+	}
+	
+	static void check(int x, int y) {
 		while(true) {
 			if(stack.isEmpty()) {
 				stack.add(new Point(x,y));
-				height = y;
 				break;
 			} else {
 				if(stack.peek().y < y) {
 					stack.add(new Point(x,y));
-					height = y;
 					break;
 				}
 				else if(stack.peek().y == y) {
 					break;
 				}
 				else {
-					height = x;
 					stack.pop();
 					count++;
 				}
 			}
 		}
-		
-		System.out.println(count);
 	}
 }
