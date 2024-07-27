7c2da5f4f6169f883c4c85504d88dfb8b6129d18
RatlyREM <39582908+RatlyREM@users.noreply.github.com>
2024-07-28 01:56:07 +0900

    [Gold III] Title: 파티, Time: 2516 ms, Memory: 45148 KB -BaekjoonHub

---
+++ 백준/Gold/1238. 파티/파티.java
@@ -0,0 +1,101 @@
+import java.util.*;
+import java.awt.Point;
+import java.io.*;
+
+public class Main {
+	static int N;
+	static int maxTotal= -1;
+	static int[][] graph;
+	static int[] visited;
+	static int[] minArr;
+	static PriorityQueue<Point> pq;
+	
+	public static void main(String[] args) throws IOException {
+		/*
+		 * 다익스트라.
+		 * 1. 먼저 출발점을 우선순위 큐에 넣고, 최단거리를 0으로 초기화
+		 * 2. 주변 노드들을 탐색하고, 바꿀 수 있는거 바꾸기. 바뀌면 큐에 넣기
+		 * 3. 우선 순위 큐에서 하나 빼기. 주변을 살피며 갱신되는게 있으면 큐에 넣기
+		 */
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		int X = Integer.parseInt(st.nextToken());
+		
+		visited = new int[N+1];
+		minArr = new int[N+1];
+		graph = new int[N+1][N+1];
+		pq = new PriorityQueue<Point>((o1, o2) ->  {
+			return o1.y- o2.y;
+		});
+		
+		for (int i = 0; i < M; i++) {
+			st = new StringTokenizer(br.readLine());
+			
+			graph[Integer.parseInt(st.nextToken())][Integer.parseInt(st.nextToken())] = Integer.parseInt(st.nextToken());
+		}
+		
+		int[] temp = dijk(X);
+		int[] xToI = new int[N+1];
+		
+		for (int i = 1; i <= N; i++) {
+			xToI[i] = temp[i];
+		}
+		
+		//System.out.println(Arrays.toString(xToI));
+		
+		
+		
+		for (int i = 1; i <= N; i++) {
+			//i-> X, X-> i의 최단거리 합을 재야 함.
+			if(true) {
+				int total = dijk(i)[X] + xToI[i];
+				//System.out.println(xToI[i] + " 가 X에서 i로의 최소, " + total + " 이 합");
+				//System.out.println("total: " + total);
+				maxTotal = Math.max(maxTotal, total);	
+			}
+			
+		}
+		
+		System.out.println(maxTotal);
+	}
+
+	private static int[] dijk(int i) {
+		Arrays.fill(visited, 0);
+		Arrays.fill(minArr, Integer.MAX_VALUE);
+		
+		minArr[i] = 0;
+		
+		pq.add(new Point(i,0));
+		
+		while(!pq.isEmpty()) {
+			Point temp = pq.poll();
+			//System.out.println(temp.x+"를 골랐습니다!!! 처음에");
+			visited[temp.x] = 1;
+			
+			for (int j = 1; j <= N; j++) {
+				//System.out.println(j + "에서 함!!!!!!!!!!!!!!!!!!!");
+				if(graph[temp.x][j] != 0) {
+					//System.out.println(minArr[temp.x] +"과 "+graph[temp.x][j] );
+					int t = minArr[temp.x] + graph[temp.x][j];
+					
+					if(t < minArr[j]) {
+						//System.out.println(j +"에서 " + t+"를 " + minArr[j]+ "에 대체해서 넣");
+						minArr[j] = t;
+						
+						if(visited[j] == 0)
+							pq.add(new Point(j, t));
+					}
+				}
+				
+			}
+			
+		}
+		
+		return minArr;
+	}
+
+}
