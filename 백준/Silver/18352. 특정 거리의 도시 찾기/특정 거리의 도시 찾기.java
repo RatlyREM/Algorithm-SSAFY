388b9a3953f7a88f13d8e4227522327789e68fc5
RatlyREM <39582908+RatlyREM@users.noreply.github.com>
2024-08-28 19:27:58 +0900

    [Silver II] Title: 특정 거리의 도시 찾기, Time: 1208 ms, Memory: 261908 KB -BaekjoonHub

--- 백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.java
+++ 백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.java
@@ -31,42 +31,20 @@ public class Main {
 		
 		//pq에서 X로부터의 최단 거리가 가장 작은 것부터 차례로 뽑아낸다.
 		PriorityQueue<Integer> pq = new PriorityQueue<>((o1, o2) -> Integer.compare(minArr[o1], minArr[o2]));
-		
-//		for (int i = 1; i <= N; i++) {
-//			pq.add(i);
-//		}
+
 		
 		pq.add(X);
 		
 		while(!pq.isEmpty()) {
-//			System.out.println("뽑는다!!!!!");
-//			System.out.println(Arrays.toString(minArr));
-//			
-//			System.out.println("이 중에서!!");
-//			for(int i: pq) {
-//				System.out.print(minArr[i]+ " ("+i + ") ");
-//			}
-			//
-			//.out.println();
-			int temp = pq.poll();
-//			System.out.println(pq);
-//			System.out.println(temp + "뽑힘");
-			
-			
-			
-			
+			int temp = pq.poll();	
+				
 			//연결되어 있는 정점이라면 기존 정점보다 작다면 업데이트
 			for (int i = 0; i < city[temp].size(); i++) {
 				if(minArr[city[temp].get(i)] > minArr[temp] +1) {
 					minArr[city[temp].get(i)] = minArr[temp] +1;
 					pq.offer(city[temp].get(i));
 				}
-				
-				//minArr[city[temp].get(i)] = Math.min(minArr[temp] +1, minArr[city[temp].get(i)]);
 			}
-			
-			//System.out.println("완료됨: ");
-			//System.out.println(Arrays.toString(minArr));
 		}
 		
 		StringBuilder sb = new StringBuilder();
