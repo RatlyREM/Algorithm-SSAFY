5ccfb4931e68e90b8fb87c12dba2b94dc9b31032
RatlyREM <39582908+RatlyREM@users.noreply.github.com>
2024-09-16 15:00:13 +0900

    [Gold V] Title: 물통, Time: 128 ms, Memory: 47088 KB -BaekjoonHub

--- 백준/Gold/2251. 물통/물통.java
+++ 백준/Gold/2251. 물통/물통.java
@@ -41,62 +41,37 @@ public class Main {
 		visited[0][0][C] = 1;
 		
 		while(!q.isEmpty()) {
-
-			//System.out.println(q);
-			
 			Miz temp = q.poll();
-			//System.out.println("temp: " + temp.x + " " + temp.y + " " + temp.z);
 			int a = temp.x;
 			int b = temp.y;
 			int c = temp.z;
 			
 			
-			if(a== 0) {
-//				if(!hs.contains(c)) {
-//					hs.add(c);
-//				}
-				
-				if(!li.contains(c)) li.add(c);
-				
-			}
+			if(a== 0) if(!li.contains(c)) li.add(c);
 			
 			//A->B
 			Point p = change(a,b,B);
-			//System.out.println(p.x + " " + p.y + " "+ c);
 			goBFS(p.x, p.y, c);
 
 			//A-> C
 			p = change(a,c, C);
-			//System.out.println(p.x + " " +b + " "+ p.y);
-			
 			goBFS(p.x, b, p.y);
 			
 			//B->A
 			p = change(b,a,A);
-			//System.out.println(p.y + " " + p.x + " "+ c);
-			
 			goBFS(p.y, p.x, c);
 			
 			//B->C
 			p = change(b,c,C);
-			//System.out.println(a + " " + p.x + " "+ p.y);
-			
 			goBFS(a, p.x, p.y);
 			
 			//C->A
 			p = change(c,a,A);
-			//System.out.println(p.y + " " +b + " "+ p.x);
-			
 			goBFS(p.y, b, p.x);
 			
 			//C->B
 			p = change(c,b,B);
-			//System.out.println(a + " " + p.y + " "+ p.x);
-			
 			goBFS(a, p.y, p.x);
-			
-			//다 붓되, 용량 초과 시 남은 건 출발지에 남김
-			//(출발지 상태, 도착지 상태, 도착지 최대)
 		}
 		
 		Collections.sort(li);
@@ -106,7 +81,6 @@ public class Main {
 	static void goBFS(int r, int w, int e) {
 		if(visited[r][w][e] == 0) {
 			visited[r][w][e] = 1;
-			//System.out.println(r + " " + w + " " + e + "추가!!");
 			q.add(new Miz(r,w,e));
 		}
 	}
