d0a29d8873529bc8cca52a8b259d075b34bf3e6c
RatlyREM <39582908+RatlyREM@users.noreply.github.com>
2024-09-18 13:54:59 +0900

    [Gold V] Title: 배열 돌리기 2, Time: 1036 ms, Memory: 38596 KB -BaekjoonHub

--- 백준/Gold/16927. 배열 돌리기 2/배열 돌리기 2.java
+++ 백준/Gold/16927. 배열 돌리기 2/배열 돌리기 2.java
@@ -1,87 +1,87 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 public class Main {
-    
-	static int N, M, R;
-	static int[][] map;
-    
-	static int[] dx = {0, 1, 0, -1};
-	static int[] dy = {1, 0, -1, 0};
-	
-    public static void main(String[] args) throws IOException {
-    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    	
-    	StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-    	
-    	N = Integer.parseInt(st.nextToken());
-    	M = Integer.parseInt(st.nextToken());
-    	R = Integer.parseInt(st.nextToken());
-    	
-    	map = new int[N][M];
-    	
-    	for(int i=0; i<N; i++) {
-    		st = new StringTokenizer(br.readLine(), " ");
-    		for(int j=0; j<M; j++) {
-    			map[i][j] = Integer.parseInt(st.nextToken());
-    		}
-    	}
-    	
-    	// 회전시켜야하는 덩어리 수 만큼 반복
-    	int nN = N;
-    	int nM = M;
-    	for(int i=0; i<Math.min(M, N)/2; i++) {
-    		/*
-    		 * i : 회전을 시작할 좌표
-    		 * 2*N + 2*M - 4 : 처음엔 가장 겉 테두리의 갯수, 그 다음엔 각 변 길이가 2씩 줄도록 해서 넣어줌
-    		 */
-    		rotate(i, 2*nN + 2*nM - 4);
-    		nN -= 2;
-    		nM -= 2;
-    	}
-    	
-    	print();
-    }
-
-    static void rotate(int start, int len) {
-    	
-    	// 나누기 연산을 사용하여 반복되는 반복을 최소화 해준다.
-    	int cir = R % len;
-    	
-    	// 새롭게 구해낸 회전 횟수 만큼 회전시킴
-    	for(int t=0; t<cir; t++) {
-    		
-    		int temp = map[start][start]; // 마지막에 넣을 값 미리 빼놓음
-    		
-    		int idx = 0; // 다음 값 탐색할 인덱스
-    		
-    		int x = start;
-			int y = start;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		int R = Integer.parseInt(st.nextToken());
+		
+		int[][] arr = new int[N][M];
+		
+		for (int i = 0; i < N; i++) {
+			 st = new StringTokenizer(br.readLine());
+				
+			for (int j = 0; j < M; j++) {
+				arr[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+		
+		//한 depth씩 파고들어가기
+		int t = Math.min(N, M)/2;
+		int nM = M;
+		int nN = N;
+		
+		for (int i = 0; i < t; i++) {
+			//i,i가 현재 시작지점
+			int r = R % (2*nM + 2*nN -4);
+			nM -= 2;
+			nN -= 2;
 			
-    		while(idx < 4) {
-    			
-    			int nx = x + dx[idx];
-    			int ny = y + dy[idx];
-    			
-    			if(nx >= start && ny >= start && nx < N-start && ny < M-start) {
-    				map[x][y] = map[nx][ny];
-    				x = nx;
-    				y = ny;
-    			} else {
-    				idx++;
-    			}
-    		}
-    		map[start+1][start] = temp;	// 마지막에 미리 빼놨던 값 넣엊둠
-    	}
-    	
-    }
-    
-    static void print() {
-    	for(int i=0; i<N; i++) {
-    		for(int j=0; j<M; j++) {
-    			System.out.print(map[i][j]+" ");
-    		}
-    		System.out.println();
-    	}
-    }
+			for (int a = 0; a < r; a++) {
+				int temp = arr[i][i];
+				
+				//윗줄
+				for (int j = i; j < M-i-1; j++) {
+					arr[i][j] = arr[i][j+1];
+				}
+				
+				//오른쪽 줄
+				for (int j = i; j < N-i-1; j++) {
+					arr[j][M-i-1] = arr[j+1][M-i-1];
+				}
+				
+				//밑 줄
+				for (int j = M-i-1; j >i; j--) {
+					arr[N-i-1][j] = arr[N-i-1][j-1];
+				}
+				
+				//왼쪽 줄
+				for (int j = N-i-1; j >i ; j--) {
+					if(j == i+1) {
+						arr[j][i] = temp;
+					}
+					else {
+						arr[j][i] = arr[j-1][i];
+					}
+					
+				}
+				
+				
+//				//배열 출력
+//				for (int q = 0; q < N; q++) {
+//					for (int w = 0; w < M; w++) {
+//						System.out.print(arr[q][w] + " ");
+//					}
+//					System.out.println();
+//				}
+//				System.out.println();
+			
+			}
+			
+		}
+		
+		
+		
+		//배열 출력
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < M; j++) {
+				System.out.print(arr[i][j] + " ");
+			}
+			System.out.println();
+		}
+	}
 }
